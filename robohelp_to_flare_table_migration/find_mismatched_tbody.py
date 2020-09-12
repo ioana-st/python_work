d7f6d85b1f805e6017cfdfbdb6073c00512bdefa
Ioana <st.ioana@gmail.com>
2020-09-12 17:24:08 +0300

    add script documentation

--- robohelp_to_flare_table_migration/find_mismatched_tbody.py
+++ robohelp_to_flare_table_migration/find_mismatched_tbody.py
@@ -8,7 +8,7 @@ for root, dirs, files in os.walk(path):
             file_path = os.path.join(root,file)
             with open (file_path, encoding="utf-8") as html_file:
                 html_content=html_file.read()
-                opening = html_content.count("<tbody>")
-                closing = html_content.count("</tbody>")
-                if opening != closing:
+                opening = html_content.count("<tbody>") # The number of <tbody> tags in the file.
+                closing = html_content.count("</tbody>") # The number of </tbody> tags in the file.
+                if opening != closing: # If the number of opening tags is different from the number of closing tags, there is a malformed table in the file.
                     print(file_path)
